e698cd3c9425f7d54c2a211e10db28c4e8c3c8f4
Maleeshas <81752163+Maleeshas@users.noreply.github.com>
2023-01-27 10:21:54 +0530

    updated to a self checking testbench with random inputs

--- Simple Dual Port RAM/With_interface/testbench.sv
+++ Simple Dual Port RAM/With_interface/testbench.sv
@@ -6,7 +6,7 @@ module sdpr_tb;
 
 logic clk;
 bit rst;
-
+localparam REPETITONS = 10000; // no of repitions to test
 
 localparam CLK_PERIOD = 10;
 initial begin
@@ -27,39 +27,39 @@ simple_dual_port_ram DUT(
         .ifp(ifp)
 
     );
-
-  initial begin
     
-/*  $dumpfile("dump.vcd"); $dumpvars;*/
-     #(CLK_PERIOD * 2);
+ // defined to imitate memory behavior   
+logic [ifp.DATA_WIDTH-1:0] mem_chk [ifp.MEM_DEPTH];// local memory created in testbench
+bit [ifp.ADDR_WIDTH-1:0]  addra;   
+bit [ifp.STRB_WIDTH-1:0]   wena;
+bit [ifp.DATA_WIDTH-1:0]  dina;  
+bit [ifp.ADDR_WIDTH-1:0]  addrb;   
+bit   renb;
+bit [ifp.DATA_WIDTH-1:0]  doutb;
+bit    dvalb;
+
+initial repeat(REPETITONS ) begin // random number writing to memory while writing same in the local memory created
+addra = $random ;
+dina = $random;
+#(CLK_PERIOD * 2);
 ifp.wena = 1'b1;
-  ifp.addra = 10'd5;
-  ifp.dina= 32'd350;
-  
-  #(CLK_PERIOD * 2);
-  ifp.wena = 1'b1;
-  ifp.addra = 10'd7;
-  ifp.dina= 32'd670;
-  
-  #(CLK_PERIOD * 2);
-   ifp.wena = 1'b0;
-   ifp.renb= 1'b1;
-   ifp.addrb = 10'd5;
+ifp.addra = addra;
+ifp.dina= dina;
+mem_chk[addra] = dina;  // writing to local memory
+
+end
+
+initial repeat(REPETITONS ) begin  //reading random addresses from memory and comparing it with the data in local memory 
+addrb = $random ;
+ifp.renb = 1;
+ifp.addrb = addrb;
+@(posedge clk);
+# (CLK_PERIOD * 2.5);
+a1 :assert (mem_chk[addrb]===ifp.doutb ) $display("[PASS] Addr = %0h,\n \t   Data :: Expected = %0h Actual = %0h",addrb,mem_chk[addrb],ifp.doutb);  //assertion to check whether read data is correct
+else $error("[FAIL] Addr = %0h,\n \t   Data :: Expected = %0h Actual = %0h",addrb,mem_chk[addrb],ifp.doutb);
    
-   #(CLK_PERIOD);
-    ifp.renb= 1'b1;
-    ifp.addrb = 10'd7;
-       
-   #(CLK_PERIOD *3);
-     ifp.wena = 1'b1;
-     ifp.addra = 10'd5;
-     ifp.dina= 32'd961;
-     ifp.renb= 1'b1;
-     ifp.addrb = 10'd5;
-     
-       #(CLK_PERIOD *3);
-     
-   
-   end
+
+end
+
 
 endmodule
